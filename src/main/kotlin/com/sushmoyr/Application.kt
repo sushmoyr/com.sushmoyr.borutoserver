79a26d6f891002126fdc52d31848c7131e405155
sushmoyr <sushmoyr@gmail.com>
2022-03-07 11:43:02 +0600

    initial Commit

---
+++ src/main/kotlin/com/sushmoyr/Application.kt
@@ -0,0 +1,15 @@
+package com.sushmoyr
+
+import io.ktor.application.*
+import com.sushmoyr.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+    configureKoin()
+    configureRouting()
+    configureSerialization()
+    configureMonitoring()
+}
